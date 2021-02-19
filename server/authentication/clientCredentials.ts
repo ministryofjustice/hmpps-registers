391dd17e204c506107bd295351bde2ff2af11792
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-02-19 15:14:42 +0000

    DT-1649 corrected the client credentials being used for self

--- server/authentication/clientCredentials.ts
+++ server/authentication/clientCredentials.ts
@@ -1,8 +1,8 @@
 import config from '../config'
 
 export default function generateOauthClientToken(
-  clientId: string = config.apis.hmppsAuth.apiClientId,
-  clientSecret: string = config.apis.hmppsAuth.apiClientSecret
+  clientId: string = config.apis.hmppsAuth.systemClientId,
+  clientSecret: string = config.apis.hmppsAuth.systemClientSecret
 ): string {
   const token = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
   return `Basic ${token}`
