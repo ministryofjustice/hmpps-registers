d173e30b98976d3a15acd5ebc13b608ab09e1acb
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-03-22 15:11:25 +0000

    DT-1774 rename to `add`

--- server/routes/courtRegister/courtRegisterRouter.test.ts
+++ server/routes/courtRegister/courtRegisterRouter.test.ts
@@ -22,10 +22,10 @@ describe('GET /', () => {
       })
   })
 })
-describe('GET /add-new', () => {
+describe('GET /add', () => {
   it('should render add new court start page', () => {
     return request(app)
-      .get('/court-register/add-new')
+      .get('/court-register/add')
       .expect('Content-Type', /html/)
       .expect(res => {
         expect(res.text).toContain('Add a new court')
