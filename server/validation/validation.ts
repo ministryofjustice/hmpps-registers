76bec3eea02ec49923b7317fe5ca2e198a1cc962
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-04-22 14:19:42 +0100

    DT-1773 Use validatorjs

---
+++ server/validation/validation.ts
@@ -0,0 +1,77 @@
+import Validator, { ErrorMessages, Rules } from 'validatorjs'
+import { Court, CourtBuilding } from '../@types/courtRegister'
+
+export default function validateAsync<T>(
+  form: T,
+  rules: Rules,
+  customMessages: ErrorMessages,
+  lookups: {
+    courtLookup: (subCode: string) => Promise<Court | null>
+    courtBuildingLookup: (subCode: string) => Promise<CourtBuilding | null>
+  } = {
+    courtLookup: () => null,
+    courtBuildingLookup: () => null,
+  }
+): Promise<Array<{ text: string; href: string }>> {
+  Validator.register(
+    'postcode',
+    value => {
+      if (typeof value === 'string') {
+        return !!value.replace(/[\s.,/=\-_`()]/g, '').match(/^[A-Z]{1,2}[0-9R][0-9A-Z]?[0-9][ABD-HJLNP-UW-Z]{2}$/)
+      }
+      return false
+    },
+    'Enter a real postcode, like AA11AA'
+  )
+
+  Validator.registerAsync(
+    'unique-subcode',
+    async (value, requirement, request, passes) => {
+      if (typeof value === 'string') {
+        if (!isBlank(value)) {
+          const existingCourt = await lookups.courtLookup(value)
+          if (existingCourt) {
+            passes(false, `${existingCourt.courtName} already has that code. Choose another code`)
+            return
+          }
+          const existingCourtBuilding = await lookups.courtBuildingLookup(value)
+          if (existingCourtBuilding) {
+            passes(
+              false,
+              `The court building ${existingCourtBuilding.buildingName} already has that code. Choose another code`
+            )
+            return
+          }
+        }
+      }
+      passes()
+    },
+    `The code is already used. Choose another code`
+  )
+
+  const validation = new Validator(form, rules, customMessages)
+
+  return checkErrorsAsync(validation)
+}
+const checkErrorsAsync = <T>(validation: Validator.Validator<T>): Promise<Array<{ text: string; href: string }>> => {
+  const asErrors = (errors: Validator.Errors) =>
+    Object.keys(errors.all()).map(key => {
+      const message = errors.first(key) as string
+      return { text: message, href: `#${key}` }
+    })
+
+  return new Promise(resolve => {
+    validation.checkAsync(
+      () => {
+        resolve([])
+      },
+      () => {
+        resolve(asErrors(validation.errors))
+      }
+    )
+  })
+}
+
+const isBlank = (text: string) => {
+  return !text || text.trim().length === 0
+}
