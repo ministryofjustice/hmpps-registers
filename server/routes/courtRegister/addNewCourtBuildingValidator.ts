9ffcd7feea4ca0f880208edb8b7ee501b6ea8a0b
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-04-22 18:07:00 +0100

    DT-1773 Move all building pages to new validator with a non-async version (#64)

--- server/routes/courtRegister/addNewCourtBuildingValidator.ts
+++ server/routes/courtRegister/addNewCourtBuildingValidator.ts
@@ -1,42 +1,27 @@
 import { Request } from 'express'
 import type { AddNewCourtForm } from 'forms'
-
-function isBlank(text: string) {
-  return !text || text.trim().length === 0
-}
-
-function isValidPostcode(text: string) {
-  return text.replace(/[\s.,/=\-_`()]/g, '').match(/^[A-Z]{1,2}[0-9R][0-9A-Z]?[0-9][ABD-HJLNP-UW-Z]{2}$/)
-}
+import { validate as validateSync } from '../../validation/validation'
 
 export default function validate(form: AddNewCourtForm, req: Request): string {
-  const errors: Array<{ text: string; href: string }> = []
-
-  if (isBlank(form.buildingname)) {
-    errors.push({ text: 'Enter the building name', href: '#buildingname' })
-  }
-
-  if (isBlank(form.addressline1)) {
-    errors.push({ text: 'Enter the first line of the address', href: '#addressline1' })
-  }
-
-  if (isBlank(form.addresstown)) {
-    errors.push({ text: 'Enter the town or city', href: '#addresstown' })
-  }
-
-  if (isBlank(form.addresscounty)) {
-    errors.push({ text: 'Enter the county', href: '#addresscounty' })
-  }
-
-  if (isBlank(form.addresspostcode)) {
-    errors.push({ text: 'Enter the postcode, like AA11AA', href: '#addresspostcode' })
-  } else if (!isValidPostcode(form.addresspostcode)) {
-    errors.push({ text: 'Enter a real postcode, like AA11AA', href: '#addresspostcode' })
-  }
-
-  if (isBlank(form.addresscountry)) {
-    errors.push({ text: 'Select the country', href: '#addresscountry' })
-  }
+  const errors = validateSync(
+    form,
+    {
+      buildingname: 'required',
+      addressline1: 'required',
+      addresstown: 'required',
+      addresscounty: 'required',
+      addresspostcode: ['required', 'postcode'],
+      addresscountry: 'required',
+    },
+    {
+      'required.buildingname': 'Enter the building name',
+      'required.addressline1': 'Enter the first line of the address',
+      'required.addresstown': 'Enter the town or city',
+      'required.addresscounty': 'Enter the county',
+      'required.addresspostcode': 'Enter the postcode, like AA11AA',
+      'required.addresscountry': 'Select the country',
+    }
+  )
 
   if (errors.length > 0) {
     req.flash('errors', errors)
