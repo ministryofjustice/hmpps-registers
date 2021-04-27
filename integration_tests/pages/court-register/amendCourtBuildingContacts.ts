9deb984ff0c031fb241e35b006471a1c020a717f
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-04-27 16:02:02 +0100

    DT-1772 Add validation to amend contacts (#72)

--- integration_tests/pages/court-register/amendCourtBuildingContacts.ts
+++ integration_tests/pages/court-register/amendCourtBuildingContacts.ts
@@ -3,7 +3,9 @@ import page from '../page'
 
 const contactDetails = {
   saveButton: () => cy.contains('Save'),
+  addAnotherNumberButton: () => cy.contains('Add another number'),
   removeButton: (index: number) => cy.get(`[data-qa=contact-details-section-${index}] .moj-add-another__remove-button`),
+  lastRemoveButton: () => cy.get(`.moj-add-another__remove-button`).last(),
   number: (index: number) => cy.get(`#contacts\\[${index}\\]\\[number\\]`),
   type: (index: number) => cy.get(`#contacts\\[${index}\\]\\[type\\]`),
   errorSummary: () => cy.get('.govuk-error-summary'),
