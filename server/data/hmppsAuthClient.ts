adf5e9d354837e23ec8cdfb5d00bf0bd59401403
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-05-13 16:09:24 +0100

    HAAR-428 Fix import order

--- server/data/hmppsAuthClient.ts
+++ server/data/hmppsAuthClient.ts
@@ -1,11 +1,11 @@
 import superagent from 'superagent'
+import { URLSearchParams } from 'url'
 import type TokenStore from './tokenStore'
 
 import logger from '../../logger'
 import config from '../config'
 import generateOauthClientToken from '../authentication/clientCredentials'
 import RestClient from './restClient'
-import { URLSearchParams } from 'url'
 
 const timeoutSpec = config.apis.hmppsAuth.timeout
 const hmppsAuthUrl = config.apis.hmppsAuth.url
