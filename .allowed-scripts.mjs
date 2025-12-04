8c2e36abe23d0a52adbf5885db87de0da6df6782
Dafydd Houston <159147179+dafhoustonmoj@users.noreply.github.com>
2025-12-04 09:56:26 +0000

    MAP-3068: add npm allowed scripts configuration (#338)
    * MAP-3068: add npm allowed scripts configuration
    
    Signed-off-by: dafhoustonmoj <dafydd.houston@justice.gov.uk>
    
    * MAP-3068: add npm allowed scripts configuration
    
    Signed-off-by: dafhoustonmoj <dafydd.houston@justice.gov.uk>
    
    * MAP-3068: add npm allowed scripts configuration
    
    Signed-off-by: dafhoustonmoj <dafydd.houston@justice.gov.uk>
    
    * MAP-3068: add precommit hooks
    
    * MAP-3068: add precommit hooks
    
    * MAP-3068: add precommit hooks
    
    * add precommit hook
    
    ---------
    
    Signed-off-by: dafhoustonmoj <dafydd.houston@justice.gov.uk>

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,18 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+  allowlist: {
+    // Needed to run integration tests
+    'node_modules/cypress@13.17.0': 'ALLOW',
+    // Provides native integration, supporting the ability to write dtrace probes for bunyan
+    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
+    'node_modules/esbuild@0.25.1': 'ALLOW',
+    // Needed by jest for running tests in watch mode
+    'node_modules/fsevents@2.3.3': 'ALLOW',
+    // Needed by esbuild for watching files during development
+    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    // Native solution to quickly resolve module paths, used by jest and eslint
+    'node_modules/unrs-resolver@1.10.1': 'ALLOW',
+  },
+})
