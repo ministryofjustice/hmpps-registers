df668fd34a930fea7bcf7d7673286a35c7ab6043
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2025-12-04 17:33:37 +0000

    dependency updates

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,7 +3,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // Needed to run integration tests
-    'node_modules/cypress@13.17.0': 'ALLOW',
+    'node_modules/cypress@15.7.1': 'ALLOW',
     // Provides native integration, supporting the ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
@@ -14,5 +14,6 @@ export default configureAllowedScripts({
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.10.1': 'ALLOW',
+    'node_modules/protobufjs@7.5.4': 'ALLOW',
   },
 })
