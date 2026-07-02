6dbe9548109748dd9afcafff3b64d746d359b9d0
David Lonsdale <david.lonsdale@justice.gov.uk>
2026-07-02 15:06:12 +0100

    Fixing issues found in dev
    - Updated .env.example file for running locally
    - Added override to `govuk-header` colour to keep it black
    - Added missing networks config in docker-compose
    - Fixed broken start scripts in package.json due to esbuild update
    - Fixed broken filter button due to new MOJ Frontend update

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto'
 import express, { Router } from 'express'
 import helmet from 'helmet'
 import config from '../config'
@@ -8,17 +9,21 @@ export default function setUpWebSecurity(): Router {
   // Secure code best practice - see:
   // 1. https://expressjs.com/en/advanced/best-practice-security.html,
   // 2. https://www.npmjs.com/package/helmet
-  router.use(
+  router.use((req, res, next) => {
+    res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
+    next()
+  })
+  router.use((req, res, next) =>
     helmet({
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
-          scriptSrc: ["'self'", '*.google-analytics.com'],
+          scriptSrc: ["'self'", `'nonce-${res.locals.cspNonce}'`, '*.google-analytics.com'],
           styleSrc: ["'self'", 'fonts.googleapis.com'],
           fontSrc: ["'self'", config.apis.frontendComponents.url],
         },
       },
-    }),
+    })(req, res, next),
   )
   return router
 }
